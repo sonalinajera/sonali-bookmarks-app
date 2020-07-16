eb3b3a970d1c58c879b84b04de3370d3b966f2d5
Sonali Najera <sonalitzul@gmail.com>
2020-07-15 17:18:26 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+"use strict"; 
+
+
+// ***** test to make sure js works ****  
+//function main () {
+//   handleTestClick();
+// }
+
+// function handleTestClick() {
+//   $('#test').on('click', function (event) {
+//     event.preventDefault();
+//     alert('Hey Girl');
+//   });
+// }
+
+// $(main)
